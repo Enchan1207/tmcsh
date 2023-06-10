0a2d63ac06fb870780396375345bc65d987854f2
Enchan <enchant0130@gmail.com>
2023-06-10 14:19:00 +0900

    [Update] Basic TMC shell

--- README.md
+++ README.md
@@ -6,6 +6,86 @@
  - ついでに `^C` とか `^D` とかで中断…とまではいかなくても安全に処理したい
  - スレッドでやればいいのはそれはそうだけど、ここはasyncioを使ってみよう
 
+## Discussion
+
+### 基本的なやりとり
+
+`python-usbtmc` を用いてTMCデバイスと通信する場合、基本的にはこのようなコードになる。
+
+```python
+import sys
+from typing import Optional
+import usb
+import usbtmc
+
+def main() -> int:
+    # 通信対象の選択
+    device: Optional[usb.core.Device] = choose_tmc_device()
+    if device is None:
+        return 1
+
+    # 通信路の確立
+    print("Establish connection...")
+    instrument = usbtmc.Instrument(device)
+    instrument.timeout = 2
+    instrument.open()
+
+    # コマンドのやりとり
+    end_request: bool = False
+    while not end_request:
+        try:
+            command = input(">>> ")
+            print(instrument.ask(command))
+        except (KeyboardInterrupt, EOFError):
+            end_request = True
+
+    # 終了
+    print("Closing...")
+    instrument.close()
+    return 0
+```
+
+結果:
+
+```
+% python main.py
+Connected USB-TMC devices:
+  [0] Tektronix TDS2012B (0699:0367)
+Enter the index of the device you want to communicate with.
+Type 'r' to reload, ^C or ^D to abort.
+> 0
+Establish connection...
+>>> *IDN?
+TEKTRONIX,TDS 2012B,C062851,CF:91.1CT FV:v22.01
+>>> *ESR?
+164
+>>> LANG?
+JAPANESE
+>>> Closing...
+```
+
+対話シェルっぽくやり取りできています。
+
+### タイムアウトとスレッドブロック
+
+しかし、*測定器を現在の状態に設定するのに必要な、すべてのコマンドのASCII文字列を返* すコマンド、`*LRN` などは処理に時間がかかることがあります。現状のコードでこのコマンドを実行すると、環境によってはタイムアウトで落ちてしまいます。
+
+```
+Establish connection...
+>>> *LRN?
+Unexpected exception occured in main(): [Errno 60] Operation timed out
+```
+
+タイムアウト時間を伸ばせば想定通りの応答が得られますが…
+
+```
+>>> *LRN?
+POSITION2 -6.4E0;:MEASUREMENT:MEAS1:TYPE PWIDTH;SOURCE CH1;:MEASUREMENT:MEAS2…(省略)
+```
+
+それでも、応答が得られるまで (正確には、`usbtmc.ask()`が返るまで) はメインスレッドがブロックされてしまいます。`KeyboardInterrupt` もいい感じに補足しづらいため、シェルとしても操作感がよくありません。
+
+
 ## License
 
 This repository is published under [MIT License](LICENSE).
--- main.py
+++ main.py
@@ -2,20 +2,96 @@
 #
 #
 import sys
+from typing import Optional
 import usb
 import usbtmc
 
 
 def main() -> int:
+    # 通信対象の選択
+    device: Optional[usb.core.Device] = choose_tmc_device()
+    if device is None:
+        return 1
 
+    # 通信路の確立
+    print("Establish connection...")
+    instrument = usbtmc.Instrument(device)
+    instrument.timeout = 2
+    instrument.open()
+
+    # コマンドのやりとり
+    end_request: bool = False
+    while not end_request:
+        try:
+            command = input(">>> ")
+            print(instrument.ask(command))
+        except (KeyboardInterrupt, EOFError):
+            end_request = True
+
+    # 終了
+    print("Closing...")
+    instrument.close()
     return 0
+
+
+def choose_tmc_device() -> Optional[usb.core.Device]:
+    """通信対象のTMCデバイスをユーザに選択させる
+
+    Returns:
+        Optional[usb.core.Device]: 選択されたデバイス
+
+    Note:
+        有効なデバイスが選択されるか、SIGINT(^C), EOF(^D) が入力されるまでループします。
+        デバイスが選択されないままループを抜けた場合はNoneが返ります。
+    """
+
+    selected_device: Optional[usb.core.Device] = None
+    end_request: bool = False
+    while not end_request:
+        # 一覧表示
+        print("Connected USB-TMC devices:")
+        connected_tmc_devices = usbtmc.list_devices()
+        for index, tmc_device in enumerate(connected_tmc_devices):
+            device_identifiers_str = f"{tmc_device.idVendor:04X}:{tmc_device.idProduct:04X}"
+            product_info_str = usb.util.get_string(tmc_device, tmc_device.iProduct)
+            print(f"  [{index}] {product_info_str} ({device_identifiers_str})")
+
+        # 選択させる
+        print("Enter the index of the device you want to communicate with.")
+        print("Type 'r' to reload, ^C or ^D to abort.")
+        try:
+            command = ""
+            while command == "":
+                command = input("> ")
+        except (KeyboardInterrupt, EOFError):
+            print("Abort")
+            end_request = True
+            continue
+
+        if command == "r":
+            print("Reloading..")
+            continue
+
+        try:
+            target_device_index = int(command)
+            if target_device_index < 0 or target_device_index >= len(connected_tmc_devices):
+                print("Invalid index")
+                continue
+
+            selected_device = connected_tmc_devices[target_device_index]
+            end_request = True
+        except ValueError:
+            pass
+
+    return selected_device
 
 
 if __name__ == "__main__":
     result = 0
     try:
         result = main() or 0
-    except KeyboardInterrupt:
-        print("Ctrl+C")
+    except Exception as e:
+        print(f"Unexpected exception occured in main(): {e}")
+        result = 1
     finally:
         sys.exit(result)
